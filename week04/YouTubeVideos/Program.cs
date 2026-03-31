7289f65f2f5fff43962398815bf5a0d7a70a8d20
chiiebere2026 <asoyamary@gmail.com>
2026-03-31 03:46:13 +0100

    youtubevideo-week04 project

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -5,5 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+
+         List<Video> videos = new List<Video>();
+
+        Video v1 = new Video("C# Basics", "Mary Tech", 600);
+        v1.AddComment(new Comment("John", "Great video!"));
+        v1.AddComment(new Comment("Anna", "Very helpful."));
+        v1.AddComment(new Comment("Mike", "Nice explanation."));
+
+      
+        Video v2 = new Video("Web Development", "Code Master", 900);
+        v2.AddComment(new Comment("Sarah", "Loved it!"));
+        v2.AddComment(new Comment("Paul", "Very detailed."));
+        v2.AddComment(new Comment("Grace", "Awesome content."));
+
+        Video v3 = new Video("Python Tutorial", "Dev Hub", 750);
+        v3.AddComment(new Comment("Tom", "Easy to follow."));
+        v3.AddComment(new Comment("Lucy", "Thanks for sharing."));
+        v3.AddComment(new Comment("James", "Helpful tutorial."));
+
+        videos.Add(v1);
+        videos.Add(v2);
+        videos.Add(v3);
+
+        foreach (Video video in videos)
+        {
+            video.Display();
+            Console.WriteLine("\n----------------------\n");
+        }
     }
 }
